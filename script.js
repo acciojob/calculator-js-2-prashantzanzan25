86050568bf2c542ae883a0d5f50df5b43a4e870e
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2024-04-29 09:39:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const display = document.getElementById('display');
+
+document.querySelectorAll('.buttons button').forEach(button => {
+  button.addEventListener('click', () => {
+    if (button.id === 'C') {
+      display.value = '';
+    } else if (button.id === 'back') {
+      display.value = display.value.slice(0, -1);
+    } else if (button.id === 'equal') {
+      try {
+        display.value = eval(display.value);
+      } catch (error) {
+        display.value = 'Error';
+      }
+    } else {
+      display.value += button.id;
+    }
+  });
+});
